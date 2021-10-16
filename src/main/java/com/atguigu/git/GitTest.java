675c86aeadbedab5e87583374f9bc411239c5859
TangYu <TangYu@qq.com>
2021-10-16 19:35:43 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author hxy
+ * @create 2021-10-16 19:23
+ */
+public class GitTest {
+    public static void main(String[] args){
+        System.out.println("Hello Git!");
+    }
+}
