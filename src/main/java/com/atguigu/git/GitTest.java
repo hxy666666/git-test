9a89b254b50da1dccdd43af184c25853b1a5d8e8
TangYu <TangYu@qq.com>
2021-10-17 15:24:12 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("Hello Git!");
         System.out.println("Hello Git2222!");
         System.out.println("Hello Git3333");
+        System.out.println("Hello Git4444");
     }
 }
